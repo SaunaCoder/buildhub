edd0c8ea6b98f657b44d241def5d628310fd650d
Oleksandr Krutko <oleksandr.krutko07@gmail.com>
2025-12-22 16:18:58 +0200

    Create middleware for authentication

---
+++ src/authMiddleware.js
@@ -0,0 +1,21 @@
+const admin = require('./firebase');
+
+async function authMiddleware(req, res, next) {
+    const header = req.headers.authorization;
+
+    if (!header || !header.startsWith("Bearer ")) {
+        return res.status(401).json({ error: "No token provided" });
+    }
+
+    const token = header.split(" ")[1]
+
+    try {
+        const decodedToken = await admin.auth().verifyIdToken(idToken);
+        req.user = decodedToken; // uid, email и т.д.
+        next();
+    } catch (error) {
+        return res.status(401).json({ error: "Invalid token" });
+    }
+}
+
+module.exports = authMiddleware;
