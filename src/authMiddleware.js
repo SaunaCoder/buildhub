b7c468a3d9100ef06780ac238deab170c49952c9
Oleksandr Krutko <oleksandr.krutko07@gmail.com>
2025-12-23 12:21:50 +0200

    Fix critical bug when authorization couldn't recognize token

--- src/authMiddleware.js
+++ src/authMiddleware.js
@@ -1,4 +1,4 @@
-const admin = require('./firebase');
+const { admin } = require('./firebase');
 
 async function authMiddleware(req, res, next) {
     const header = req.headers.authorization;
@@ -7,14 +7,14 @@ async function authMiddleware(req, res, next) {
         return res.status(401).json({ error: "No token provided" });
     }
 
-    const token = header.split(" ")[1]
+    const token = header.split(" ")[1];
 
     try {
-        const decodedToken = await admin.auth().verifyIdToken(idToken);
+        const decodedToken = await admin.auth().verifyIdToken(token);
         req.user = decodedToken; // uid, email и т.д.
         next();
     } catch (error) {
-        return res.status(401).json({ error: "Invalid token" });
+        return res.status(401).json({ error: error.message });
     }
 }
 
