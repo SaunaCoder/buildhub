ae7f423c7a4250ccc43919fb20719b5759cf4458
Oleksandr Krutko <oleksandr.krutko07@gmail.com>
2025-12-23 12:23:56 +0200

    Remove commented legacy code and import that is not longer used

--- src/authorVerification.js
+++ src/authorVerification.js
@@ -1,17 +1,3 @@
-const admin = require('./firebase');
-
-/*async function authorVerificationMiddleware(req, res, next) {
-    const user_id = req.user.uid;
-    const author_id = req.params.author_id;
-
-    if (user_id != author_id) {
-        return res.status(403).json({ err: "You are not the author!" })
-    }
-
-    next()
-    
-}*/
-
 async function authorVerification(old_doc, user_id) {
     const snapshot = await old_doc.get();
 
